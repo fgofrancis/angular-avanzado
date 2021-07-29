264f5379dcd99e371fce6c0b6496adc979d00917
Francis Figuereo <fgocam@gmail.com>
2021-07-29 17:22:45 -0400

    Fin seccion 4 - Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
+
 import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages/pages.component';
-import { ProgressComponent } from './pages/progress/progress.component';
+
 
 const routes: Routes = [
-  { path: '', component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'grafica1', component: Grafica1Component },
-      { path: '', redirectTo:'/dashboard', pathMatch: 'full'},
-    ]
+
+  {
+    path:'auth',
+    loadChildren: ()=> import('./auth/auth.module').then(m => m.AuthModule)
   },
-
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-
-  { path: '**',component: NopagefoundComponent }
+  {
+    path:'dashboard',
+    loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)
+  },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '**', component: NopagefoundComponent}
 ];
 
 @NgModule({
